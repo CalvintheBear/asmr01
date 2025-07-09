13acd3566ed03dae38208ff7e1864bc901594adb
CalvintheBear <j2983236233@gmail.com>
2025-07-10 02:03:19 +0800

    Update EnhancedFeedbackButton.tsx

--- src/components/EnhancedFeedbackButton.tsx
+++ src/components/EnhancedFeedbackButton.tsx
@@ -21,7 +21,7 @@ export default function EnhancedFeedbackButton({ onClick, isMobile = false, clas
       >
         <MessageCircle className="w-4 h-4 animate-bounce-subtle" />
         <span>Feedback</span>
-        <span className="text-xs text-white ml-1 opacity-75">
+        <span className="text-xs text-white ml-1">
           (30 Credits Reward)
         </span>
       </button>
